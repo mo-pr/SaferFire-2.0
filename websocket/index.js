b51e0a24461d8e95cdc6c11193eeb3940bfd0d22
mo-pr <preiningmoritz@gmail.com>
2021-12-27 22:03:06 +0100

    alarm api request working/ login working / token not checked yet/  no exception handling

--- websocket/index.js
+++ websocket/index.js
@@ -1,5 +1,7 @@
 //region IMPORTS
 var methods = require('./methods')
+var jwt = require('jsonwebtoken')
+var crypto = require('crypto');
 var express = require('express');
 var expressWs = require('express-ws');
 var expressWs = expressWs(express());
@@ -7,21 +9,33 @@ var app = expressWs.app;
 var wss = expressWs.getWss('/');
 //endregion
 
+var prime_length = 60;
+var diffHell = crypto.createDiffieHellman(prime_length);
+diffHell.generateKeys('base64');
+
 app.ws('/login',(ws,req)=>{
     ws.on('message', msg => {
-        console.log(msg);
         var jsonmsg = JSON.parse(msg);
-        ws.send(methods.login(jsonmsg['username'], jsonmsg['password']));
+        methods.login(jsonmsg['username'], jsonmsg['password'],diffHell.getPrivateKey(),function (token) {
+            ws.send(token)
+        });
     })
 })
 
 app.ws('/alarm',(ws,req)=>{
-    ws.on('message',msg=>{
+    ws.on('message',msg => {
         var jsonmsg = JSON.parse(msg);
-        console.log(methods.alarm(jsonmsg['firedep']));
-        wss.clients.forEach(function broadcast(client){
-            client.send('Alarm '+msg);
-        })
+        try {
+            jwt.verify(jsonmsg['token'], diffHell.getPublicKey(), {algorithm: 'HS256'});
+        }catch{
+            ws.close()
+        }
+        methods.alarm(function (alarms) {
+            console.log(alarms['cnt_einsaetze'])
+            wss.clients.forEach(function broadcast(client) {
+                client.send(JSON.stringify(alarms))
+            })
+        });
     })
 })
 
--- websocket/methods.js
+++ websocket/methods.js
@@ -1,17 +1,62 @@
 var XMLHttpRequest = require('xhr2');
+const { Client } = require('pg');
+var jwt = require('jsonwebtoken')
+var crypto = require('crypto');
+
+var prime_length = 60;
+var diffHell = crypto.createDiffieHellman(prime_length);
+diffHell.generateKeys('base64');
+
+const client = new Client({
+    user: 'dev',
+    host: IP,
+    database: 'saferfire',
+    password: 'dev',
+    port: 5432,
+})
+client.connect()
 
 module.exports={
-    login: function (username, passwd){
-        return username+"\n"+passwd;
+    login: function (uname, passwd, privateK, _call) {
+        var token = undefined;
+        client.query("SELECT * from missiondata where LOWER(location) like LOWER('abc')", (err, res) => {
+            if (res.rows[0] === undefined) {
+                console.log('No user found!')
+            }
+            else{
+                let payload = {
+                    username: 'abc',
+                };
+                token = jwt.sign(payload, privateK, {expiresIn: "15d", algorithm: 'HS256'})
+                console.log(token)
+                _call(token)
+            }
+        })
     },
-    alarm: function (firedep){
-        const Http = new XMLHttpRequest();
-        const url='https://cf-intranet.ooelfv.at/webext2/rss/json_2tage.txt';
-        Http.open("GET", url);
-        Http.send();
-        Http.onreadystatechange = (e) => {
-            console.log(Http.responseText)
-        }
-        return 'ALARM '+firedep;
+    alarm: function (_call){
+        getJSON('https://cf-intranet.ooelfv.at/webext2/rss/json_2tage.txt',
+            function(err, data) {
+                if (err !== null) {
+                    console.log('Something went wrong: ' + err);
+                } else {
+                    //console.log(data);
+                    _call(data);
+                }
+        });
     }
+};
+
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        var status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
 };
