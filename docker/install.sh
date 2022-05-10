cd5383c335ea88cba4d0e41ea84ee72a2fe15483
Moritz Preining <preiningmoritz@gmail.com>
2022-05-10 20:32:00 +0200

    GA_24 SaferFire

--- docker/install.sh
+++ docker/install.sh
@@ -6,6 +6,10 @@ ACCESS_TOKEN=$2
 echo $ACCESS_TOKEN | docker login ghcr.io --username $GITHUB_USER --password-stdin
 
 docker-compose pull
-docker-compose run -d -p 3030:3030 saferfire-backend
+docker-compose run -d -p 3030:3030 --name SaferFire saferfire-backend
+docker exec -it SaferFire bash
+pm2 start ./dist/main.js
+pm2 startup
+pm2 save 
 
 exit
