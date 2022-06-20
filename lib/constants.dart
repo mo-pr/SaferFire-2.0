1d0cb7b6b9cf94d6dc0263165b317255987ae566
unknown <alessandro.detta2004@gmail.com>
2022-06-20 06:04:35 +0200

    Solve merge conflicts

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,13 +6,8 @@ import 'package:saferfire/models/Protocol.dart';
 import 'alarm.dart';
 
 List<Alarm> alarms = [];
-<<<<<<< HEAD
 Color buttonColor = Color(0xeFFB82626);
 Protocol protocol = new Protocol(null, null, null, null, null, null);
-String ipAddress = "140.238.211.240";
 bool isProtocol = false;
-bool isTest = true;
-=======
 String ipAddress = "140.238.211.240";
 bool isTest = true;
->>>>>>> cc8915c467a8ace0084ac9ebc79412bf24a88edd
