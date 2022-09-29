59606e81f8f94b587f3bc04e151db0e799c1692e
Moritz Preining <preiningmoritz@gmail.com>
2022-09-29 17:18:44 +0200

    Changed Port to 80

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,6 +4,6 @@ import 'alarm.dart';
 
 List<Alarm> alarms = [];
 Protocol protocol = new Protocol(null, null, null, null, null);
-String ipAddress = "140.238.211.240";
+String ipAddress = "152.67.71.8";
 bool isProtocol = false;
 bool isTest = true;
