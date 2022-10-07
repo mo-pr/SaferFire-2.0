1f4ef74244feed52c98d2545088ac8e943161a3f
unknown <alessandro.detta2004@gmail.com>
2022-10-07 09:52:52 +0200

    Added upload to firebase

--- lib/camerafunction.dart
+++ lib/camerafunction.dart
@@ -1,8 +1,37 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 openCamera() async {
   final image = await ImagePicker().pickImage(source: ImageSource.camera);
 }
 openGallery() async {
   final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+}
+uploadImage(PickedFile image) async {
+  final _firebaseStorage = FirebaseStorage.instance;
+  //Check Permissions
+  await Permission.photos.request();
+
+  var permissionStatus = await Permission.photos.status;
+
+  if (permissionStatus.isGranted){
+    //Select Image
+    var file = File(image.path);
+
+    if (image != null){
+      //Upload to Firebase
+      var snapshot = await _firebaseStorage.ref()
+          .child('images/imageName')
+          .putFile(file);
+      var downloadUrl = await snapshot.ref.getDownloadURL();
+    } else {
+      print('No Image Path Received');
+    }
+  } else {
+    print('Permission not granted. Try Again with permission access');
+  }
 }
