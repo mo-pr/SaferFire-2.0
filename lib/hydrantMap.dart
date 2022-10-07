1dcb75a5fd292aeb9b9fe3e543f404ad83a1751d
mo-pr <preiningmoritz@gmail.com>
2022-10-07 15:11:52 +0200

    Added Hydrant Map

---
+++ lib/hydrantMap.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HydrantMap extends StatelessWidget {
+  HydrantMap({Key? key}) : super(key: key);
+  final Completer<WebViewController> _controller = Completer<WebViewController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: "https://portal.wasserkarte.info/watermap/waterSources/view?all=1&tpl=map",
+      javascriptMode: JavascriptMode.unrestricted,
+      onWebViewCreated: (WebViewController webViewController){
+        _controller.complete(webViewController);
+      }
+    );
+  }
+}
