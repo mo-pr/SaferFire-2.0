ae758ecdfd05989a11cd70cf9d599c32073bc67d
Preining Moritz <preiningmoritz@gmail.com>
2022-10-12 11:36:01 +0200

    Added API endpoint

---
+++ websocket/src/dangerous-goods/Dto.ts
@@ -0,0 +1,4 @@
+export class DangerousGoodsDto{
+    token:string;
+    unNo:number;
+}
---
+++ websocket/src/dangerous-goods/dangerousGood.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, HttpCode, HttpException, HttpStatus, Logger, Post } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Connection} from "typeorm";
+import { DangerousGoodsDto } from "./Dto";
+
+@Controller('dangerousgoods')
+export class DangerousGoodsController{
+    constructor(private conn: Connection, private jwtService:JwtService) {}
+
+    private logger:Logger = new Logger('DangerousController');
+
+    @Post()
+    @HttpCode(200)
+    @HttpCode(400)
+    @HttpCode(401)
+    async allalarmsdb(@Body() payload:DangerousGoodsDto) {
+        var dGood;
+        if(this.jwtService.decode(payload.token)['user'] == "Admin"){
+            try{
+                dGood = await this.readDangerousGoodFromDatabase(payload.unNo);
+            }
+            catch(err){
+                throw new HttpException('Alarms could not be read from database',HttpStatus.BAD_REQUEST);
+            }
+            this.logger.log('Alarms read from database');
+            return dGood;
+        }
+        else{
+            throw new HttpException('Access denied',HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    public async readDangerousGoodFromDatabase(unNo:number){
+        var goods
+        this.logger.log("Reading substance from Database");
+        const qRunner = this.conn.createQueryRunner();
+        await qRunner.connect();
+        await qRunner.query(`SELECT * FROM dangerousgoods WHERE un_nr = '${unNo}'`).then(x => goods=x);
+        await qRunner.release();
+        return goods;
+    }
+}
