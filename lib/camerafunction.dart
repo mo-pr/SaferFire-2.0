82356b2d2292bed306d77ae5164e16d3a35dcfb2
unknown <alessandro.detta2004@gmail.com>
2022-10-14 09:50:55 +0200

    Fix firebase

--- lib/camerafunction.dart
+++ lib/camerafunction.dart
@@ -3,16 +3,16 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
-
+import 'package:path/path.dart' as p;
 openCamera() async {
   await Permission.photos.request();
   final image = await ImagePicker().pickImage(source: ImageSource.camera);
-  uploadImage(image!);
+  await uploadImage(image!);
 }
 openGallery() async {
   await Permission.location.request();
   final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-  uploadImage(image!);
+  await uploadImage(image!);
 }
 //https://www.educative.io/answers/how-to-upload-to-firebase-storage-with-flutter
 uploadImage(XFile image) async {
@@ -22,7 +22,8 @@ uploadImage(XFile image) async {
     if (image != null){
       //Upload to Firebase
       var snapshot = await _firebaseStorage.ref()
-          .child('images/imageName')
+          .child('images')
+          .child(p.basename(file.path))
           .putFile(file);
       var downloadUrl = await snapshot.ref.getDownloadURL();
     } else {
