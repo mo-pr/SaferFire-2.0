38174daaac34827d3933d2f429635d83fb4b36ba
unknown <alessandro.detta2004@gmail.com>
2022-12-13 10:03:56 +0100

    Improve Loading Time and Change Icon

--- lib/hydrantMap.dart
+++ lib/hydrantMap.dart
@@ -11,35 +11,68 @@ import 'models/Hydrant.dart';
 
 class HydrantMap extends StatelessWidget {
   HydrantMap({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      title: "Hydrantmap",
+      home: StatefulHydrantWidget()
+    );
+  }
+}
+
+class StatefulHydrantWidget extends StatefulWidget{
+  const StatefulHydrantWidget({super.key});
+  @override
+  State<StatefulHydrantWidget> createState() => _StatefulHydrantWidgetState();
+}
+
+class _StatefulHydrantWidgetState extends State<StatefulHydrantWidget>{
   MapController controller = MapController(
       initMapWithUserPosition: true
   );
-
-  Widget build(BuildContext context) {
-    var response = getCurrentPostion().then((value) =>  getHydrants(value.longitude, value.latitude)).then((value) => convertHydrantResponse(value.body));
-    return OSMFlutter(
-      controller:controller,
-      trackMyPosition: true,
-      initZoom: 15,
-      markerOption: MarkerOption(
-          defaultMarker: MarkerIcon(
-            icon: Icon(
-              Icons.person_pin_circle,
-              color: Colors.blue,
-              size: 70,
-            ),
-          )
-      ),
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        body:
+        Stack(
+          children: <Widget>[
+          FutureBuilder<List<Hydrant>>(
+              future: loadMarkers(),
+              builder: (BuildContext buildContext, AsyncSnapshot<List<Hydrant>> snapshot){
+                return OSMFlutter(
+                  controller:controller,
+                  trackMyPosition: true,
+                  initZoom: 15,
+                  markerOption: MarkerOption(
+                      defaultMarker: MarkerIcon(
+                        icon: Icon(
+                          Icons.person_pin_circle,
+                          color: Colors.blue,
+                          size: 70,
+                        ),
+                      )
+                  ),
+                );
+              }
+          ),
+          ],
+        ),
     );
-  }
 
+  }
+  Future<List<Hydrant>> loadMarkers() async{
+    var currentPostion = await getCurrentPostion();
+    var hydrants = await getHydrants(currentPostion.longitude, currentPostion.latitude);
+    return await convertHydrantResponse(hydrants.body);
+  }
   Future<Position> getCurrentPostion() async{
     return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
   }
 
   Future<http.Response> getHydrants(double lng, double lat) async{
-    return http.get(
-      Uri.parse('https://api.wasserkarte.info/1.0/getSurroundingWaterSources/?source=$source&token=$token&lat=$lat&lng=$lng&range=$range&numItems=$numItems'),
+    var uri = Uri.parse('https://api.wasserkarte.info/1.0/getSurroundingWaterSources/?source=$source&token=$token&lat=${lat.toStringAsFixed(2)}&lng=${lng.toStringAsFixed(2)}&range=$range&numItems=$numItems');
+    return await http.get(
+      uri
     );
   }
   Future<List<Hydrant>> convertHydrantResponse(String response) async{
@@ -47,10 +80,16 @@ class HydrantMap extends StatelessWidget {
     var waterSources = hydrants['waterSources'];
     List<Hydrant> allHydrants = [];
     for(var singleWaterSource in waterSources){
-     var newHydrant = Hydrant(singleWaterSource['name'], singleWaterSource['id'].toString(), singleWaterSource['address'], singleWaterSource['longitude'], singleWaterSource['latitude']);
-     allHydrants.add(newHydrant);
-     var markerIcon = new MarkerIcon(icon: Icon(Icons.fire_hydrant,color: Colors.blue,size:200));
-     await controller.addMarker(new GeoPoint(latitude: newHydrant.lat!, longitude: newHydrant.lng!), markerIcon:  markerIcon);
+      var newHydrant = Hydrant(
+          singleWaterSource['name'],
+          singleWaterSource['id'].toString(),
+          singleWaterSource['address'],
+          singleWaterSource['longitude'],
+          singleWaterSource['latitude'],
+          singleWaterSource['iconUrl']
+      );
+      var myMarkerIcon = new MarkerIcon(icon: new Icon(Icons.location_pin, color: Colors.blue, size: 150));
+      await controller.addMarker(new GeoPoint(latitude: newHydrant.lat!, longitude: newHydrant.lng!), markerIcon: myMarkerIcon);
     }
     return allHydrants;
   }
